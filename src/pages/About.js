457ed7e9a1b8414916ef3d10e8e490759085481b
Hashim Irfan <hashimirfan99@gmail.com>
2021-07-16 22:54:03 +0530

    committed

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,7 +18,7 @@ class About extends React.Component {
                     <img src={mypic} className="photo" />
                     <div className="intro-brief">
                         <p id="about-content">
-                            {`UI/UX Designer, passionate about solving problems and designing intuitive digital experiences that leave a positive impact on people and the society.\n\nI’m a speedcuber, an amateur guitarist, a terrible singer (not being modest) but I like to sing. I prefer indie folk music & yes! I love AR Rahman :3\n\nJust completed my bachelor’s from Crescent University Chennai, I’m a self-learnt designer drawing experiences by solving real world problems for the users around me.`}
+                            {`UI/UX Designer, passionate about solving problems and designing intuitive digital experiences that leave a positive impact on people and the society.\n\nI’m into cryptocurrency, cat lover, a fitness freak. I prefer indie folk music & yes! I love AR Rahman <3 \n\nJust completed my bachelor’s from Crescent University Chennai, I’m a self-learnt designer drawing experiences by solving real world problems for the users around me.`}
                         </p>
                     </div>
                     <p id="quote">Engineer by Qualification, Designer by <span>Choice.</span></p>
